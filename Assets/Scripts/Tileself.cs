7b7af90b00b63bd737c6377efad379f3a259695e
walklook <walklook@hotmail.com>
2014-09-26 02:39:38 +0800

    Update Tileself.cs

--- Assets/Scripts/Tileself.cs
+++ Assets/Scripts/Tileself.cs
@@ -33,20 +33,20 @@ public class Tileself : MonoBehaviour
 	void Update ()
 	{
 		bool isDelete = false;
-        Vector3 pos = transform.position;
-        if ( pos.x < TileNavigation.sTopSide )
+		Vector3 pos = transform.position;
+		if ( pos.x < TileNavigation.sTopSide )
 		{
 			isDelete = true;
 		}
-        else if ( pos.x > TileNavigation.sBottomSide )
+		else if ( pos.x > TileNavigation.sBottomSide )
 		{
 			isDelete = true;
 		}
-        else if ( pos.z < TileNavigation.sLeftSide )
+		else if ( pos.z < TileNavigation.sLeftSide )
 		{
 			isDelete = true;
 		}
-        else if ( pos.z > TileNavigation.sRightSide )
+		else if ( pos.z > TileNavigation.sRightSide )
 		{
 			isDelete = true;
 		}
@@ -55,19 +55,19 @@ public class Tileself : MonoBehaviour
 		{
 			lock ( TileNavigation.sTerrainRecords )
 			{
-                for ( int j = 0; j < TileNavigation.sTerrainRecords.Count; j++ )
+				for ( int j = 0; j < TileNavigation.sTerrainRecords.Count; j++ )
 				{
 					TileNavigation.TerrainIndex tr = (TileNavigation.TerrainIndex)TileNavigation.sTerrainRecords[j];
 
 					if ( tr.col == mCol && tr.row == mRow )
 					{
-                        TileNavigation.sTerrainRecords.RemoveAt( j );
+			                        TileNavigation.sTerrainRecords.RemoveAt( j );
 						break;
 					}
 				}
 			}
 		    
-            // Because the purpose of multithread loading terrain is for memory limit, so don't cache them. besides this, every terrain object may have different state in games.
+			// Because the purpose of multithread loading terrain is for memory limit, so don't cache them. besides this, every terrain object may have different state in games.
 			DestroyImmediate( gameObject );
 			Resources.UnloadUnusedAssets();
 		}
